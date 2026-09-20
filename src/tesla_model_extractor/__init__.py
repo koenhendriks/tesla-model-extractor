c5a6204d644ebb8a9403538b83008e235a48165b
Koen Hendriks <info@koenhendriks.nl>
2026-09-20 14:58:21 +0200

    release: 0.3.1

--- src/tesla_model_extractor/__init__.py
+++ src/tesla_model_extractor/__init__.py
@@ -1,6 +1,6 @@
 """tesla-model-extractor: the 3D vehicle models, materials and animations of the Tesla app, from a bundle you own."""
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 PACK_FORMAT = 1
 GDRE_VERSION = "2.6.4"
